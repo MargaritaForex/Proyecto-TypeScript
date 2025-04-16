e7607b04f7b9131f91d3cba6ac62e2673fb9a037
milindr4123 <e.foreros@uniandes.edu.co>
2025-04-15 23:12:11 -0500

    tercer tarea

--- main.ts
+++ main.ts
@@ -1,39 +1,4 @@
-let op1: number = 10;
-let op2: number = 15;
+import { Aprendiz, NivelEducativo } from "./aprendiz";
 
-console.log(`Mis variables iniciales son ${op1} y ${op2}`);
-
-op1++;
-op2 += 4;
-
-console.log(`Mis variables finales son ${op1} y ${op2}`);
-
-let resultado: number = op1 / op2;
-console.log(`Resultado: ${resultado}`);
-console.log(`Resultado: ${resultado.toPrecision(2)}`);
-
-let varTipoDesconocido: any;
-console.log(`La variable tiene valor: ${varTipoDesconocido} y es de tipo ${typeof varTipoDesconocido}`);
-varTipoDesconocido = "Ahora hay un texto";
-console.log(`La variable tiene valor: ${varTipoDesconocido} y es de tipo ${typeof varTipoDesconocido}`);
-varTipoDesconocido = 2.8;
-console.log(`La variable tiene valor: ${varTipoDesconocido} y es de tipo ${typeof varTipoDesconocido}`);
-
-function imprimirMensaje(): void {
-    console.log("Mensaje genérico");
-}
-
-function imprimirMensaje2(msj: string): void {
-    console.log(msj);
-}
-
-imprimirMensaje();
-imprimirMensaje2("Mensaje parámetro");
-
-function sumar(op1: number, op2: number): number {
-    return op1 + op2;
-}
-
-let fsumar = sumar;
-console.log(fsumar(10, 15));
-console.log(fsumar(20, 25));
+export const ap =  new Aprendiz("Margarita", "Forero", "avatar.png", 30, NivelEducativo.POSGRADO);
+console.log(ap);
